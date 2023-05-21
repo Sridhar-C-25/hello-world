73036702a547de991245a73894b63ec41a2ace71
Code A Program <75136330+Sridhar-C-25@users.noreply.github.com>
2023-05-21 18:14:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.send({msg:"hello world"}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
